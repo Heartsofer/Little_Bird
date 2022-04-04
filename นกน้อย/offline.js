7c56ffc479ae0232715d0ca2b3ac718008645a0f
Heartsofer <102953716+Heartsofer@users.noreply.github.com>
2022-04-04 15:59:33 +0700

    that

---
+++ นกน้อย/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1648974135,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipeb-sheet0.png",
+		"images/pipet-sheet0.png",
+		"images/tiledbackground.png",
+		"images/pictimer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
